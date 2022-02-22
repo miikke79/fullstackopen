bcd2ee3b60d7e8e6541c2c9386150ec761b89d24
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-02-22 15:28:44 +0200

    part 1a step2

--- part1/src/App.js
+++ part1/src/App.js
@@ -11,15 +11,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
   <div>
-  <p>
-    {props.part1} {props.exercises1}
-  </p>
-  <p>
-    {props.part2} {props.exercises2}
-  </p>
-  <p>
-    {props.part3} {props.exercises3}
-  </p>
+  <Part part={props.part1} exercise ={props.exercises1} />
+  <Part part={props.part2} exercise ={props.exercises2} />
+  <Part part={props.part3} exercise ={props.exercises3} />
 </div>
   )
 }
@@ -29,6 +23,16 @@ const Total = (props) => {
   <div>
   <p>
     Number of exercises {props.total}
+  </p>
+</div>
+  )
+}
+
+const Part = (props) => {
+  return (
+  <div>
+  <p>
+    {props.part} {props.exercise}
   </p>
 </div>
   )
