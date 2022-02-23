a048c25913e1fab505f6cdefbdbfa81394dd01fb
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-02-23 15:44:45 +0200

    part 1d step8

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -6,16 +6,17 @@ const Button = (props) => (
   </button>
 )
 
-const Display = (props) => (
+const Statistics = (props) => (
 
-<div>
-  <p>good {props.good}</p>
-  <p>neutral {props.neutral}</p>
-  <p>bad {props.bad}</p>
-  <p>all {props.all}</p>
-  <p>average {props.average}</p>
-  <p>positive {props.positive} %</p>
-</div>
+  <div>
+   <p>good {props.good}</p>
+   <p>neutral {props.neutral}</p>
+   <p>bad {props.bad}</p>
+   <p>all {props.all}</p>
+   <p>average {props.average}</p>
+   <p>positive {props.positive} %</p>
+  </div>
+  
 )
 
 const App = () => {
@@ -36,7 +37,7 @@ const App = () => {
       <Button handleClick={() => {setNeutral(neutral+1), setAll(all+1), setAverage((good-bad)/all), setPositive((good/all)*100)}} text="neutral" />
       <Button handleClick={() => {setBad(bad + 1), setAll(all+1), setAverage((good-bad)/all), setPositive((good/all)*100)}} text="bad" />
       <h1>statistics</h1>
-      <Display good={good} neutral={neutral} bad={bad} all= {all} average= {average} positive= {positive} />
+      <Statistics good={good} neutral={neutral} bad={bad} all= {all} average= {average} positive= {positive} />
     </div>
     
   )
