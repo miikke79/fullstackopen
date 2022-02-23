1924952862b02344573088ea23bc96220adbeb32
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-02-23 20:07:44 +0200

    part 1d step11

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -7,7 +7,9 @@ const Button = (props) => (
 )
 
 const StatisticLine = (props) => (
-  <p>{props.text} {props.value}</p>
+  <tr>
+    <td>{props.text}</td><td>{props.value}</td>
+  </tr>
 )
 
 const Statistics = (props) => {
@@ -16,12 +18,18 @@ const Statistics = (props) => {
   }
 
   else if (props.all === 1) {
-    return (<div>
-        <StatisticLine text="good" value ={props.good} />
-        <StatisticLine text="neutral" value ={props.neutral} />
-        <StatisticLine text="bad" value ={props.bad} />
-        <StatisticLine text="all" value ={props.all} />
-      </div>)
+    return (
+      <div>
+        <table>
+          <tbody>
+           <StatisticLine text="good" value ={props.good} />
+           <StatisticLine text="neutral" value ={props.neutral} />
+           <StatisticLine text="bad" value ={props.bad} />
+           <StatisticLine text="all" value ={props.all} />
+         </tbody>
+       </table>
+     </div>
+    )
   }
 
   else {
@@ -29,12 +37,16 @@ const Statistics = (props) => {
     return(
 
   <div>
-    <StatisticLine text="good" value ={props.good} />
-    <StatisticLine text="neutral" value ={props.neutral} />
-    <StatisticLine text="bad" value ={props.bad} />
-    <StatisticLine text="all" value ={props.all} />
-    <StatisticLine text="average" value ={props.average} />
-    <StatisticLine text="positive" value ={props.positive} />
+    <table>
+        <tbody>
+          <StatisticLine text="good" value ={props.good} />
+          <StatisticLine text="neutral" value ={props.neutral} />
+          <StatisticLine text="bad" value ={props.bad} />
+          <StatisticLine text="all" value ={props.all} />
+          <StatisticLine text="average" value ={props.average} />
+          <StatisticLine text="positive" value ={props.positive} />
+        </tbody>
+    </table>
   </div>
 )
     }
